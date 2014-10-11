4909662b36bf38555e927c9e56b460ac5b79f508
jzzhu <jzzhu@zjft.com>
2014-10-11 16:13:24 +0800

    初始版本

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Squeaker::Application.config.secret_token = '56959db5896799e55096cfffaf8604b04a401962ccdc2da3f31fd8bc748a6fe13cec3950ebab9b401239809285bc1575ded549e945c571a45347d2788ecb82eb'
---
+++ features/support/factories.rb
@@ -0,0 +1,11 @@
+require 'factory_girl'
+
+FactoryGirl.define do
+  factory :user do |f|
+    f.username 'testuser'
+  end
+  factory :message do |f|
+    f.association :user
+    f.content 'Test message content'
+  end
+end
